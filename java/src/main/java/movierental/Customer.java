d0b4a7afea800b635a1bb5b7c7698a5267b3e85e
Alexandre Jeambrun <alexandre.jeambrun@octo.com>
2021-02-16 17:15:04 +0100

    primitive obsession on customer class

--- java/src/main/java/movierental/Customer.java
+++ java/src/main/java/movierental/Customer.java
@@ -2,63 +2,158 @@ package movierental;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Customer {
+class Name {
+    private final String name;
 
-    private String _name;
-    private List<Rental> _rentals = new ArrayList<Rental>();
-
-    public Customer(String name) {
-        _name = name;
-    }
-
-    public void addRental(Rental arg) {
-        _rentals.add(arg);
+    public Name(String name) {
+        this.name = name;
     }
 
     public String getName() {
-        return _name;
+        return name;
+    }
+}
+
+class Rentals {
+    private final List<Rental> rentals = new ArrayList<>();
+
+    public void add(Rental rental) {
+        rentals.add(rental);
+    }
+
+    public List<Rental> all() {
+        return rentals;
+    }
+}
+
+class Amount {
+    private final double amount;
+
+    private Amount(double amount) {
+        this.amount = amount;
+    }
+
+    public Amount add(Amount addedAmount) {
+        return of(this.amount + addedAmount.amount);
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(amount);
+    }
+
+    public static Amount of(double amount) {
+        return new Amount(amount);
+    }
+
+    public static Amount zero() {
+        return of(0.);
+    }
+}
+
+class FrequentRenterPoints {
+    private final int points;
+
+    private FrequentRenterPoints(int points) {
+        this.points = points;
+    }
+
+    public FrequentRenterPoints increment() {
+        return new FrequentRenterPoints(points+1);
+    }
+
+    public static FrequentRenterPoints zero() {
+        return new FrequentRenterPoints(0);
+    }
+
+    public String toString() {
+        return Integer.toString(points);
+    }
+}
+
+class Statement {
+    private final StringBuilder statement;
+
+    private Statement(StringBuilder statement) {
+        this.statement = statement;
+    }
+
+    public Statement append(String toAppend) {
+        return new Statement(this.statement.append(toAppend));
+    }
+
+    public String toString() {
+        return statement.toString();
+    }
+
+    public static Statement empty() {
+        return new Statement(new StringBuilder());
+    }
+}
+
+public class Customer {
+
+    private final Name name;
+    private final Rentals rentals = new Rentals();
+
+    public Customer(String name) {
+        this.name = new Name(name);
+    }
+
+    public void addRental(Rental arg) {
+        rentals.add(arg);
+    }
+
+    public String getName() {
+        return name.getName();
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        String result = "Rental Record for " + getName() + "\n";
+        Amount totalAmount = Amount.zero();
+        FrequentRenterPoints frequentRenterPoints = FrequentRenterPoints.zero();
+        Statement result = Statement.empty();
+        result = result.append("Rental Record for " + getName() + "\n");
 
-        for (Rental each: _rentals) {
-            double thisAmount = 0;
+        for (Rental each: rentals.all()) {
+            Amount thisAmount = Amount.zero();
 
             //determine amounts for each line
             switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
+                case Movie.REGULAR -> {
+                    thisAmount = thisAmount.add(Amount.of(2));
+                    if (each.getDaysRented() > 2) {
+                        final var addedAmount = Amount.of((each.getDaysRented() - 2) * 1.5);
+                        thisAmount = thisAmount.add(addedAmount);
+                    }
+                }
+                case Movie.NEW_RELEASE -> {
+                    final var newReleaseAmount = Amount.of(each.getDaysRented() * 3);
+                    thisAmount = thisAmount.add(newReleaseAmount);
+                }
+                case Movie.CHILDRENS -> {
+                    thisAmount = thisAmount.add(Amount.of(1.5));
+                    if (each.getDaysRented() > 3) {
+                        final var childrenAmount = Amount.of((each.getDaysRented() - 3) * 1.5);
+                        thisAmount = thisAmount.add(childrenAmount);
+                    }
+                }
             }
 
             // add frequent renter points
-            frequentRenterPoints++;
+            frequentRenterPoints = frequentRenterPoints.increment();
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
+                frequentRenterPoints = frequentRenterPoints.increment();
 
             // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result = result.append("\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n");
+            totalAmount = totalAmount.add(thisAmount);
         }
 
         // add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result = result.append("Amount owed is " + totalAmount + "\n");
+        result = result.append("You earned " + frequentRenterPoints + " frequent renter points");
 
-        return result;
+        return result.toString();
     }
 }
